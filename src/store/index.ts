e86f6df4bff620c93bd8eb67b0774c7baec5f362
AcWink <2576669293@qq.com>
2022-03-31 17:36:41 +0800

    feat: login main
    vuex store
    
    003

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: () => {
+    return {
+      name: "acwink"
+    };
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+});
+
+export default store;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
+const path = require('path');
+
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: './build',
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   };
+  // }
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('@components', '@/components');
+  }
 });
